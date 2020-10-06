27e794b1cdff2780f6003cccda5c0853fc048454
Johan <username@users.noreply.github.com>
2020-10-06 21:35:59 +0200

    complete annotation script and create yolo annotations

--- node_scripts/reformat_annotations.js
+++ node_scripts/reformat_annotations.js
@@ -1,17 +1,51 @@
-const fs = require('fs'); 
+const fs = require('fs');
+const path = require('path'); 
 const parser = require('fast-xml-parser');
 
-const myFile = fs.readFileSync('./DSC00559.xml', {encoding:'utf8', flag:'r'});
-const jsonObj = parser.parse(myFile);
+const inputPath = path.join(__dirname, '../images/annotations');
+const outputPath = path.join(__dirname, '../images/annotations/txt');
 
-console.log(jsonObj);
-console.log(jsonObj.annotation);
-console.log(jsonObj.annotation.object[0].name);
+fs.readdir(inputPath, (error, files) => {
+  if (error) {
+    console.error('FUUUK', error);
+  }
+  files.forEach((fileName) => {
 
-jsonObj.annotation.object.forEach(object => {
-  const objectClass = object.name;
-  const xValue = object.bndbox.x
-
-
-  console.log(`${object.name} ${object.bndbox.xmin}  ;
+    if (fileName.toLowerCase().endsWith('.xml')) {
+      const myFile = fs.readFileSync(`${inputPath}/${fileName}`, {encoding:'utf8', flag:'r'});
+      const jsonObj = parser.parse(myFile);
+      
+      const { width: imgWidth, height: imgHeight } = jsonObj.annotation.size;
+      
+      let str = '';
+      
+      if (jsonObj.annotation.object) {
+        let annotations;
+        if (!Array.isArray(jsonObj.annotation.object)) {
+          annotations = [jsonObj.annotation.object];
+        } else {
+          annotations = jsonObj.annotation.object;
+        }
+        annotations.forEach(object => {
+          const { xmin, ymin, xmax, ymax } = object.bndbox;
+          const { name } = object;
+        
+          const xcenter = (xmin + xmax) / 2 / imgWidth;
+          const ycenter = (ymin + ymax) / 2 / imgHeight;
+          const width = (xmax - xmin) / imgWidth;
+          const height = (ymax - ymin) / imgWidth;
+        
+          const result = `${name} ${xcenter} ${ycenter} ${width} ${height}`;
+          str += `${result}\n`;
+        });
+      fs.writeFile(`${outputPath}/${fileName.replace('.xml', '.txt')}`, str, 'utf8', () => {});
+      console.log('Wrote annotations for', fileName);
+      } else {
+        console.error('Cannot find annotations for', fileName);
+      }
+    }
+  });
 });
+
+
+
